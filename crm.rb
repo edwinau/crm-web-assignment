026b2c150c9ea29bc71968036126913f3702e56b
EDWIN AU <edwinau@rogers.com>
2017-04-04 18:23:28 -0400

    views created

--- crm.rb
+++ crm.rb
@@ -1,3 +1,31 @@
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
+require_relative "contact"
+require "sinatra"
+
+Contact.create("Edwin", "Au", "edwinau@rogers.com","CEO")
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+Contact.create("Greg", "Boone", "edwinau@rogers.com","CEO")
+
+get "/" do
+  @crm_app_name = "Edwin's CRM"
+  @server_time = Time.now
+  erb :index
+end
+
+get "/contacts" do
+  erb :contacts
+end
+
+get "/new_contact" do
+  erb :new_contact
+end
+
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
+  puts params
+end
